73760c3804af17969918f06ceb52b8c697b66411
Vishesh Arora <77064412+vishesh-arora@users.noreply.github.com>
2026-06-07 12:06:06 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ["pdf-parse"],
-  experimental: {
-    nodeMiddleware: true,
-  },
 };
 
 module.exports = nextConfig;
